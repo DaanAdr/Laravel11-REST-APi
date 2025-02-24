aab6e43aac2e6c17e37b17dd324eebfa322f09ba
DaanAdr <parsa1815@outlook.com>
2025-02-24 14:52:01 +0100

    Use DatabaseTransactions to prevent data from persisting into my database

--- tests/Feature/AgeRangeControllerTest.php
+++ tests/Feature/AgeRangeControllerTest.php
@@ -2,17 +2,31 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
 class AgeRangeControllerTest extends TestCase
 {
-    use RefreshDatabase;
-    
+    use DatabaseTransactions;
+
     public function test_index_returns_all_age_ranges()
     {
         $response = $this->get('/api/v1/age_range');
 
         $response->assertStatus(200);
+    }
+
+    public function test_store_creates_new_age_range()
+    {
+        $data = ['age_range' => '10-20'];
+
+        $response = $this->postJson('/api/v1/age_range', $data);
+
+        $response->assertStatus(201)
+                 ->assertJsonFragment($data);
+
+        $this->assertDatabaseHas('age_ranges', $data);
+
+        $getty = $this->getJson('/api/v1/age_range');
     }
 }
