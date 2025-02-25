e7eac4706e48daa6f62f1587bca67011d7775afe
DaanAdr <parsa1815@outlook.com>
2025-02-25 11:38:36 +0100

    Added Feature tests for Destroy endpoint

--- tests/Feature/AgeRangeControllerTest.php
+++ tests/Feature/AgeRangeControllerTest.php
@@ -17,8 +17,6 @@ class AgeRangeControllerTest extends TestCase
         $age_range_one = AgeRange::create([$age_range_prop => '5-8']);
         $age_range_two = AgeRange::create([$age_range_prop => '9-13']);
 
-        //AgeRange::factory()->count(5)->create();
-
         // Act
         $response = $this->get('/api/v1/age_range');
         $responseContent = json_decode($response->getContent(), true);
@@ -34,14 +32,18 @@ class AgeRangeControllerTest extends TestCase
     public function test_store_creates_new_age_range()
     {
         // Arrange
-        $data = ['age_range' => '10-20'];
+        $age_range_prop = 'age_range';
+        $data = [$age_range_prop => '10-20'];
 
         // Act
         $response = $this->postJson('/api/v1/age_range', $data);
+        $responseContent = json_decode($response->getContent(), true);
 
         // Assert
         $response->assertStatus(201)
-                 ->assertJsonStructure(['id', 'age_range']);
+                 ->assertJsonStructure(['id', $age_range_prop]);
+
+        $this->assertEquals($data[$age_range_prop], $responseContent[$age_range_prop]);
 
         $this->assertDatabaseHas('age_ranges', $data);
     }
@@ -53,12 +55,48 @@ class AgeRangeControllerTest extends TestCase
 
         // Act
         $response = $this->postJson('/api/v1/age_range', $data);
-        $responseContent = $response->getContent();
+        $responseContent = json_decode($response->getContent() , true);
 
         // Assert
         $response->assertStatus(422);
         $this->assertDatabaseMissing('age_ranges', $data);
-        $this->assertEquals('The age range field is required.', $$responseContent['errors']['age_range'][0]);
+        $this->assertEquals('The age range field is required.', $responseContent['errors']['age_range'][0]);
+    }
+    #endregion
+
+    #region Destroy
+    public function test_destroy_removes_age_range_from_database()
+    {
+        // Arrange
+        $age_range_prop = 'age_range';
+        $age_range_one = [$age_range_prop => '5-8'];
+        $age_range = AgeRange::create($age_range_one);
+        AgeRange::create([$age_range_prop => '9-13']);
+        $id_to_delete = $age_range->id;     // Pick the ID of the newly created item, as RefreshDatabase doesn't reset the auto-increment
+
+        // Act
+        $response = $this->delete("/api/v1/age_range/$id_to_delete");
+        $responseContent = $response->getContent();
+        $responseBool = filter_var($responseContent, FILTER_VALIDATE_BOOLEAN);
+
+        // Assert
+        $response->assertStatus(200);
+        $this->assertDatabaseMissing('age_ranges', $age_range_one);
+        $this->assertTrue($responseBool);
+    }
+
+    public function test_destory_with_incorrect_id_returns_404_not_found()
+    {
+        // Arrange
+        $age_range_prop = 'age_range';
+        AgeRange::create([$age_range_prop => '5-8']);
+        AgeRange::create([$age_range_prop => '9-13']);
+
+        // Act
+        $response = $this->delete('api/v1/age_range/103');
+
+        // Assert
+        $response->assertStatus(404);
     }
     #endregion
 }
